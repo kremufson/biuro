53f0216b80c5c4f74bea24c3ca8354ab2b0fb497
RAFALEK\Rafalek <kremufka69420@gmail.com>
2024-10-09 23:03:24 +0200

    Dodano reszte zawartości strony

--- biuro.php
+++ biuro.php
@@ -39,13 +39,25 @@ mysqli_close($db);
             </tr>
         </table>
     </div>
-    <div id="srodek"></div>
+    <div id="srodek">
+        <h2>W tym roku jedziemy do...</h2>
+            <?php foreach($obrazy as $obraz): ?>
+                <img src="<?= $obraz['nazwaPliku'] ?>" alt="<?= $obraz['podpis'] ?>" title="<?= $obraz['podpis'] ?>" />
+            <?php endforeach; ?>
+        </div>
     <div id="prawy">
         <h2>Kontakt</h2>
         <a href="mailto:biuro@wycieczki.pl">napisz do nas</a>
         <p>telefon: 444555666</p>
     </div>
-    <div id="dane"><h3>W poprzednich latach byliśmy...</h3></div>
+    <div id="dane">
+        <h3>W poprzednich latach byliśmy...</h3>
+        <ol>
+            <?php foreach($wycieczki as $wycieczka): ?>
+                <li>Dnia <?= $wycieczka['dataWyjazdu'] ?> pojechaliśmy do <?= $wycieczka['cel'] ?></li>
+            <?php endforeach ?>
+        </ol>
+    </div>
     <div id="stopka"><a>Stronę wykonał: Rafał Lietzau</a></div>
 </body>
 </html>
